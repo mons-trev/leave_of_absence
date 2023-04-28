57875768ce99d857122c08ff7bf74275b89b0e7f
변지은 <bje5774@gmail.com>
2023-04-28 18:21:49 +0900

    settings

---
+++ src/main/java/me/april/AprilApplication.java
@@ -0,0 +1,13 @@
+package me.april;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/april/AprilApplicationTests.java
@@ -0,0 +1,13 @@
+package me.april;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
